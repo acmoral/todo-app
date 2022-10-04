4008ef73f466ce10b5fd1a968e7a8fbb9ebbfd61
acmoral <acmoral@unal.edu.co>
2022-10-03 20:33:43 -0500

    added react effect

--- src/App.js
+++ src/App.js
@@ -12,33 +12,66 @@ import "./styles/footer.css"
 import "./styles/right-container.css"
 
 function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+  // Creamos el estado inicial para nuestros errores y carga
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
   
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
+  React.useEffect(() => {
+  // Simulamos un segundo de delay de carga 
+    setTimeout(() => {
+      // Manejamos la tarea dentro de un try/catch por si ocurre algún error
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+        
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
 
-  const [item, setItem] = React.useState(parsedItem);
-
+        setItem(parsedItem);
+      } catch(error) {
+      // En caso de un error lo guardamos en el estado
+        setError(error);
+      } finally {
+        // También podemos utilizar la última parte del try/cath (finally) para terminar la carga
+        setLoading(false);
+      }
+    }, 1000);
+  });
+  
   const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
+    // Manejamos la tarea dentro de un try/catch por si ocurre algún error
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch(error) {
+      // En caso de algún error lo guardamos en el estado
+      setError(error);
+    }
   };
 
-  return [
+  // Para tener un mejor control de los datos retornados, podemos regresarlos dentro de un objeto
+  return {
     item,
     saveItem,
-  ];
+    loading,
+    error,
+  };
 }
 
 function App() {
-  
-  const [todos,setTodos] = useLocalStorage('TODOS_V2', []);
+  // Desestructuramos los nuevos datos de nustro custom hook
+  const {
+    item: todos,
+    saveItem: setTodos,
+    loading,
+    error,
+  } = useLocalStorage('TODOS_V2', []);
   const [valueToadd,setvalueToadd] = React.useState('');
   const [searchValue,setSearchValue]=React.useState('');
   let newTodos = todos.filter( element => element.text.toLowerCase().includes(searchValue.toLowerCase()));
@@ -60,6 +93,9 @@ function App() {
         searchValue={searchValue}
         setSearchValue={setSearchValue}/>
         <TodoList todos={todos} newTodos = {newTodos} setTodos={setTodos}/>
+        {error && <p>Desespérate, hubo un error...</p>}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {(!loading && !todos.length) && <p>¡Crea tu primer TODO!</p>}
       </div>
       </main>
       <footer>
